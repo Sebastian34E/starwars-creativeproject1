b5c978743a1d4945a26edf13473d36e02543e43b
Sebastian Estrada <Sebastian34@berkeley.edu>
2025-03-03 11:01:31 -0800

    Removed runtime

--- OgTrilogy/svelte.config.js
+++ OgTrilogy/svelte.config.js
@@ -3,8 +3,7 @@ import adapter from '@sveltejs/adapter-vercel';
 export default {
   kit: {
     adapter: adapter({
-      // Specify a supported runtime version:
-      runtime: 'nodejs18.x'
+      
     })
   }
 };
